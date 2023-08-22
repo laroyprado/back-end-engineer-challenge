b4d8af530a21212afd0c691cffddedcb00936670
Gabriel Pavão <gabpavao@outlook.com>
2023-08-22 09:10:53 -0300

    chore: import ConfigModule to access the enviroment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ModulesModule } from './user/modules/modules.module';
-import { ModulesModule } from './users/modules/modules.module';
-import { UsersModule } from './modules/users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ModulesModule, UsersModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [],
   providers: [],
 })
