e234451efaa2bd2ded96fe29c20046f004841bae
Gabriel Pavão <gabpavao@outlook.com>
2023-08-22 09:52:20 -0300

    chore: config GraphQLModule e set its type definitions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,42 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import {
+  DateTimeResolver,
+  EmailAddressResolver,
+  JWTResolver,
+  UUIDResolver,
+  URLResolver,
+} from 'graphql-scalars';
+import { join } from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      resolvers: {
+        DateTime: DateTimeResolver,
+        EmailAddress: EmailAddressResolver,
+        URL: URLResolver,
+        UUID: UUIDResolver,
+        JWT: JWTResolver,
+      },
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'interface',
+        customScalarTypeMapping: {
+          DateTime: 'Date',
+          EmailAddress: 'string',
+          URL: 'string',
+          UUID: 'string',
+          JWT: 'string',
+        },
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
