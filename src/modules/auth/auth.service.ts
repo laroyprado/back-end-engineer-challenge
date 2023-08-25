a5df86f8923f96a51ea0e78ee7785f3411624905
Gabriel Pavão <gabpavao@outlook.com>
2023-08-25 15:46:34 -0300

    feat: create login and validateUser methods in the AuthService class

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,11 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { UsersService } from '../users/users.service';
+import { compare } from 'bcryptjs';
+import { PrismaService } from 'src/db/prisma.service';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private readonly usersService: UsersService,
+    private readonly prismaService: PrismaService,
     private readonly jwtService: JwtService,
   ) {}
+
+  async validateUser(userEmail: string, userPassword: string) {
+    const user = await this.prismaService.user.findUnique({
+      where: { email: userEmail },
+    });
+
+    if (user) {
+      const isValidPassword: boolean = await compare(
+        userPassword,
+        user.password,
+      );
+
+      if (isValidPassword) {
+        return {
+          id: user.id,
+          name: `${user.firstName} ${user.lastName}`,
+        };
+      }
+    }
+
+    return null;
+  }
+
+  async login(email: string) {
+    const user = await this.prismaService.user.findUnique({ where: { email } });
+
+    return {
+      token: this.jwtService.sign(
+        {
+          name: `${user.firstName} ${user.lastName}`,
+        },
+        {
+          subject: user.id,
+        },
+      ),
+    };
+  }
 }
