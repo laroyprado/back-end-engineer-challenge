a7b3a8c47a25d99032685095a5050154b0d2f2b6
Gabriel Pavão <gabpavao@outlook.com>
2023-09-01 10:48:38 -0300

    feat: handle unauthorized errors in jwt

--- src/modules/auth/guards/jwt-auth.guard.ts
+++ src/modules/auth/guards/jwt-auth.guard.ts
@@ -1,11 +1,24 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { ExecutionContext, HttpStatus, Injectable } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
+import { AppError } from 'src/constants/app-error.class';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
     return ctx.getContext().req;
+  }
+
+  handleRequest(err: any, user: any, info: any) {
+    console.log(err, user, info);
+
+    if (err || info) {
+      const error = info.toString().split(': ');
+
+      throw new AppError(error[0], error[1], HttpStatus.UNAUTHORIZED);
+    }
+
+    return user;
   }
 }
